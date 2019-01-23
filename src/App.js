5e5c1777a63b2e20d9d815a072468c697aee55eb
RFJC21 <rfj.cabeca@gmail.com>
2019-01-23 22:58:24 +0000

    23jan

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Switch, Route } from "react-router";
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
 class App extends Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} />
+        </Switch>
+      </React.Fragment>
+    );
   }
 }
 
